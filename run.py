50766ae55090637715606c35b90d16f0e102ab6c
Patrick Hackman <pajohack@gmail.com>
2024-03-04 17:56:18 +0000

    Add logic to handle invalid user input

--- run.py
+++ run.py
@@ -1,10 +1,18 @@
 import random
 
 number_to_guess = random.randint(1, 10)
-print(number_to_guess)
+# print(number_to_guess)
 count_number_tries = 1
 
-guess = int(input('Please guess a number between 1 and 10: '))
+def get_user_guess(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print('Please enter a valid number between 1 and 10.')
+            
+
+guess = get_user_guess('Please guess a number between 1 and 10: ')
 
 while number_to_guess != guess:
     print('Sorry, wrong number.')
