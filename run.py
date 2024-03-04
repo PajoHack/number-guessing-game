00a94b550f76da9799a488fb1870711a36e8b614
Patrick Hackman <pajohack@gmail.com>
2024-03-04 13:26:25 +0000

    Add logic that checks if the players guess is lower or higher than the number to guess

--- run.py
+++ run.py
@@ -2,5 +2,19 @@ import random
 
 number_to_guess = random.randint(1, 10)
 
-#print(number_to_guess)
+# print(number_to_guess)
+count_number_tries = 1
+guess = int(input('Please guess a number between 1 and 10: '))
 
+while number_to_guess != to guess:
+    print('Sorry, wrong number')
+    # tbd
+    if count_number_tries == 4:
+        break
+    elif guess < number_to_guess:
+        print('The number is lower than your guess.')
+    else:
+        print('The number is higher than your guess')
+        
+    guess = int(input('Please guess again: '))
+    count_number_tries += 1
