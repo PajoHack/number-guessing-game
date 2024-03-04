86c88309b4d868e1ab903a045d89dc177ab7bd41
Patrick Hackman <pajohack@gmail.com>
2024-03-04 15:21:51 +0000

    Game logic complete

--- run.py
+++ run.py
@@ -1,20 +1,28 @@
 import random
 
 number_to_guess = random.randint(1, 10)
-
-# print(number_to_guess)
+print(number_to_guess)
 count_number_tries = 1
+
 guess = int(input('Please guess a number between 1 and 10: '))
 
-while number_to_guess != to guess:
-    print('Sorry, wrong number')
-    # tbd
+while number_to_guess != guess:
+    print('Sorry, wrong number.')
+
     if count_number_tries == 4:
         break
     elif guess < number_to_guess:
-        print('The number is lower than your guess.')
+        print('Go higher!')
     else:
-        print('The number is higher than your guess')
+        print('Go lower!')
         
     guess = int(input('Please guess again: '))
     count_number_tries += 1
+    
+    
+if number_to_guess == guess:
+    print('Well done you won!')
+    print('You took', count_number_tries, 'goes to complete the game')
+else:
+    print('Sorry - you loose')
+    print('The number you needed to guess was', number_to_guess)
